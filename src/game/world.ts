f89e71179b27c43b675430fa879a9cb52c92d8b5
Riley <waskerdu@gmail.com>
2024-10-24 21:33:49 -0700

    started room camera

--- src/game/world.ts
+++ src/game/world.ts
@@ -6,6 +6,7 @@ import { HEIGHT, WIDTH } from "./globals";
 import { AABB } from "../libs/core/aabb";
 import { Vec2 } from "../libs/core/la";
 import { Player } from "./player";
+import { clamp } from "../libs/core/math";
 
 export class World{
     rooms: Set<Room>;
@@ -15,9 +16,11 @@ export class World{
     sectorWidthCells: number;
     sectorHeightCells: number;
     camera: Camera;
+    cameraSpeed = 300;
     brown: Graphics.Texture;
     wall: Graphics.Texture;
     player: Player;
+    currentSector?: Sector;
     constructor(rooms: Set<Room>, sectorLookup: HashGrid2D<Sector | undefined>, cellWidthPx: number, sectorWidthCells: number, sectorHeightCells: number){
         this.rooms = rooms;
         this.sectorLookup = sectorLookup;
@@ -29,6 +32,7 @@ export class World{
         this.wall = Graphics.Texture.fromColor(14, 14, 100, 100, 100, 255);
         this.player = new Player(this);
         this.player.position.x = WIDTH/2; this.player.position.y = HEIGHT/2;
+        this.camera.position = this.player.position.copy();
         this.collisionLookup = new HashGrid2D(0);
         for (const sector of sectorLookup.data.values()) {
             if(!sector) continue;
@@ -36,7 +40,40 @@ export class World{
             this.addSectorCollision(sector);
         }
     }
+    private shouldUpdate(dt: number): boolean{
+        // Check if camera is inbounds
+        // If it's not move it towards a valid position
+
+        // Check if current sector is defined. If not do nothing.
+        if(!this.currentSector) return true;
+        const sw = this.sectorWidthCells*this.cellWidthPx;
+        const sh = this.sectorHeightCells*this.cellWidthPx;
+        const room = this.currentSector.room;
+        const camMinX = room.posX * sw + WIDTH/2;
+        const camMinY = room.posY * sh + HEIGHT/2;
+        const camMaxX = (room.posX + room.width) * sw - WIDTH/2;
+        const camMaxY = (room.posY + room.height) * sh - HEIGHT/2;
+        const camTargetX = clamp(this.camera.position.x, camMinX, camMaxX);
+        const camTargetY = clamp(this.camera.position.y, camMinY, camMaxY);
+        const vec = new Vec2(camTargetX - this.camera.position.x, camTargetY - this.camera.position.y);
+        const speed = this.cameraSpeed *dt;
+        if(vec.length() < speed){
+            this.camera.position.x = camTargetX;
+            this.camera.position.y = camTargetY;
+            return true;
+        }
+        this.camera.position.addMutate(vec.normalizeMutate().mulMutate(speed));
+        // System.println(vec.x, vec.y);
+        return false;
+    }
     update(dt: number){
+        // if the camera is out of bounds, move it towards inbounds
+        // otherwise update player etc
+        // get player's current sector
+        const sx = Math.floor(this.player.position.x / this.sectorWidthCells / this.cellWidthPx);
+        const sy = Math.floor(this.player.position.y / this.sectorHeightCells / this.cellWidthPx);
+        this.currentSector = this.sectorLookup.get(sx, sy);
+        if(!this.shouldUpdate(dt)) return;
         this.player.update(dt);
     }
     draw(){
@@ -126,6 +163,7 @@ export class World{
             const roomWidthCells = roomWidthSectors * sectorWidthCells;
             const roomXSectors = level.worldX / sectorWidthPx;
             const roomYSectors = level.worldY / sectorHeightPx;
+            const room = {posX: roomXSectors, posY: roomYSectors, width: roomWidthSectors, height: roomHeightSectors, properties: new Map()};
             const layers = level.layerInstances;
             if(!layers) throw 'validation error';
             const intGrid = layers[0].intGridCsv;
@@ -136,12 +174,12 @@ export class World{
                 const sy = Math.floor(cy / sectorHeightCells) + roomYSectors;
                 let sector = sectorLookup.get(sx, sy);
                 if(!sector){
-                    sector = {posX: sx, posY: sy, cells: []};
+                    sector = {posX: sx, posY: sy, cells: [], room};
                     sectorLookup.set(sx, sy, sector);
                 }
                 sector.cells.push(intGrid[idx]);
             }
-            rooms.add({posX: roomXSectors, posY: roomYSectors, width: roomWidthSectors, height: roomHeightSectors, properties: new Map()});
+            rooms.add(room);
         }
         return new World(rooms, sectorLookup, cellWidthPx, sectorWidthCells, sectorHeightCells);
     }
@@ -160,4 +198,5 @@ interface Sector{
     posY: number;
     cells: number[];
     texture?: Graphics.Texture;
+    room: Room;
 }
